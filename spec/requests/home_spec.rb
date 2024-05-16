4c00d631e54ca78758828ddce92a46d2a8c30105
Zhenya Telyukov <telyukov@gmail.com>
2024-05-16 13:25:52 +0300

    home routes spec

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "Homes", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/home/index"
+RSpec.describe "Home Routing", type: :request do
+  for locale in ['de', 'en', 'ru'] do
+    it "returns http success #{locale}" do
+      get "/#{locale}"
       expect(response).to have_http_status(:success)
     end
   end
 
+  it "returns route error for fr" do
+    get "/fr"
+    expect(response.status).to eq(404)
+  end
 end
