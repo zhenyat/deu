49deaddea4ac1b40d28d25ae7d6c83c07037502b
Zhenya Telyukov <telyukov@gmail.com>
2024-05-16 12:06:03 +0300

    dropdown corrected

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,5 +2,5 @@
 import { Application } from '@hotwired/stimulus'
 import Dropdown from '@stimulus-components/dropdown'
 
-const application = Application.start()
-application.register('dropdown', Dropdown)
+// const application = Application.start()
+// application.register('dropdown', Dropdown)
