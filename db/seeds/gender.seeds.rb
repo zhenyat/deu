c220707d1bd3b1da197ca8621673e31f0060eac1
Zhenya Telyukov <telyukov@gmail.com>
2024-05-21 14:21:31 +0300

    some updates

--- db/seeds/gender.seeds.rb
+++ db/seeds/gender.seeds.rb
@@ -1,10 +1,10 @@
 begin
   if (Gender.present? and not Gender.exists?)
     Gender.create!([
-      {de: '-',                     en: 'any',       ru: '-',     abbr_de: '-', abbr_en: '-', abbr_ru: '-'},
-      {de: 'maskulinum / männlich', en: 'masculine', ru: 'м.р.',  abbr_de: 'M', abbr_en: 'M', abbr_ru: 'М'},
-      {de: 'femininum / weiblich',  en: 'feminine',  ru: 'ж.р.',  abbr_de: 'F', abbr_en: 'F', abbr_ru: 'Ж'},
-      {de: 'neutrum / sächlich',    en: 'neuter',    ru: 'ср.р.', abbr_de: 'N', abbr_en: 'N', abbr_ru: 'Ср'}
+      {de: 'sonst                   en: 'other',     ru: 'прочие',  abbr_de: '-', abbr_en: '-', abbr_ru: '-'},
+      {de: 'männlich / maskulinum', en: 'masculine', ru: 'м.р.',    abbr_de: 'M', abbr_en: 'M', abbr_ru: 'М'},
+      {de: 'weiblich / femininum',  en: 'feminine',  ru: 'ж.р.',    abbr_de: 'F', abbr_en: 'F', abbr_ru: 'Ж'},
+      {de: 'sächlich / neutrum',    en: 'neuter',    ru: 'ср.р.',   abbr_de: 'N', abbr_en: 'N', abbr_ru: 'Ср'}
     ])
     puts "===== #{Gender.count} 'Gender' record(s) created"
   else
