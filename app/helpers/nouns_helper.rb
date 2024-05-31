3a84b7c0d4f88430ad344f666260b95c9e1e90b0
Zhenya Telyukov <telyukov@gmail.com>
2024-05-31 16:47:41 +0300

    colorized noun

--- app/helpers/nouns_helper.rb
+++ app/helpers/nouns_helper.rb
@@ -28,6 +28,20 @@ module NounsHelper
     Article.find_by(gcase: Gcase.find_by(en: 'nominative'), number: Number.find_by(en: 'singular'),gender: noun.gender).de
   end
 
+  def show_noun_colorized noun
+    case noun.gender.abbr_en
+    when 'M'
+      klass = "text-blue-700 font-bold"
+    when 'F'
+      klass = "text-red-700 font-bold"
+    when 'N'
+      klass = "text-green-700 font-bold"
+    else
+      klass = "text-yellow-700 font-bold"
+    end
+    content_tag(:div, content_tag(:div, show_article(noun) + " " + noun.de + " " + fine_transcription(noun.transcription)), class: klass)
+  end
+
   def show_noun_de noun
     content_tag(:div, content_tag(:div, show_article(noun) + " " + noun.de + " " + fine_transcription(noun.transcription)), class: "border-2", style: "color: blue")
   end
