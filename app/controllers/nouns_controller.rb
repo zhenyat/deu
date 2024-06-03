3d643e680e4efc20ae384002588f147dde0cd390
Zhenya Telyukov <telyukov@gmail.com>
2024-06-03 17:00:32 +0300

    form styles update

--- app/controllers/nouns_controller.rb
+++ app/controllers/nouns_controller.rb
@@ -61,46 +61,6 @@ class NounsController < ApplicationController
   end
 
   private
-
-    # def declension_check
-    #   gender_id = params['noun']['gender_id']
-    #   gender_other_id = Gender.find_by(en: 'other').id
-    #   singular_number_id = Number.find_by(en: 'singular').id
-    #   plural_number_id = Number.find_by(en: 'plural').id
-    #   puts "======= ZT1  gender_id: #{gender_id}, gender_other_id: #{gender_other_id}, singular_number_id: #{singular_number_id}, plural_number_id: #{plural_number_id}"
-    #   params['noun']["noun_declensions_attributes"].each do |declension|
-    #     puts "----- ZT2 last-gcase_id: #{declension.last['gcase_id']}, last-singular: #{declension.last['singular']}, last-plural: #{declension.last['plural']}"
-    #     gcase_id = declension.last['gcase_id']
-    #     article_singular = declension.last['singular'].split().first
-    #     article_plural = declension.last['plural'].split().first
-    #     puts "+++++ ZT3 gcase_id: #{gcase_id}, article_singular: #{article_singular}, article_plural: #{article_plural}"
-    #     if(
-    #       article_singular != Article.find_by(
-    #         gcase_id: gcase_id, number_id: singular_number_id, gender_id: gender_id).de
-    #     )
-    #       redirect_to :new, status: :unprocessable_entity
-    #       # return {status: 'error', message: "Incorrect singular article for case: #{gcase.en}"}
-    #       # @noun.errors.add(:base, "Article is not correct: #{article_singular} (#{Gcase.find(gcase_id).de} / singular)") 
-    #     end
-  
-    #     if(
-    #       article_plural != Article.find_by(
-    #         gcase_id: gcase_id, number_id: plural_number_id, gender_id: gender_other_id).de
-    #     )
-    #       redirect_to :new, status: :unprocessable_entity
-    #       # return {status: 'error', message: "Incorrect plural article for case: #{gcase.en}"}
-    #     # @noun.errors.add(:base, "Article is not correct: #{article_plural} (#{Gcase.find(gcase_id).de} / plural)")
-    #     end
-    #     return{status: 'ok'}
-    #   end
-
-      # result = @noun.declension_articles_check
-      # if result[:status] == 'error'
-      #   format.html { render :new, status: result[:message] }
-      # return {status: 'error', message: "Incorrect plural article for case: #{gcase.en}"}
-      # end
-    # end
-
     # Use callbacks to share common setup or constraints between actions.
     def set_noun
       @noun = Noun.find(params[:id])
